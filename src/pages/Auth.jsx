f0124809e8aa42df0e1f2f4bba490dfb4b8ae655
k228719 <k228719@nu.edu.pk>
2025-12-05 11:48:37 +0500

    Checking if the changes happened automically apart from the Home.jsx file

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -18,7 +18,7 @@ const Auth = () => {
           className={`px-4 py-2 rounded-r ${!isLogin ? 'bg-blue-600 text-white' : 'bg-gray-200'}`}
           onClick={() => setIsLogin(false)}
         >
-          Sign Up
+          Register
         </button>
       </div>
       {isLogin ? <LoginForm /> : <SignupForm />}
