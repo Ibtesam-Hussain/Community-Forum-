043f2d22b3f3f437e9ed2ab5261fd71cef47f550
Ibtesam-Hussain <ibtesamh786@gmail.com>
2025-12-05 16:19:25 +0500

    changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
   const navigate = useNavigate();
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-4xl font-bold mb-6">Welcome to Community Q&A Forum</h1>
+      <h1 className="text-4xl font-bold mb-6">Welcome to Community Q&A Forum by azhar ul islam</h1>
       <button
         className="bg-green-600 text-white px-6 py-3 rounded text-lg hover:bg-blue-700"
         onClick={() => navigate("/auth")}
