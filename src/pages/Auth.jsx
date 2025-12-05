212a54afa246f8f380888247709d82377f455646
k228719 <k228719@nu.edu.pk>
2025-12-05 17:49:20 +0500

    Updating Auth.jsx

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -18,7 +18,7 @@ const Auth = () => {
           className={`px-4 py-2 rounded-r ${!isLogin ? 'bg-blue-600 text-white' : 'bg-gray-200'}`}
           onClick={() => setIsLogin(false)}
         >
-          Register
+          Sign Up
         </button>
       </div>
       {isLogin ? <LoginForm /> : <SignupForm />}
