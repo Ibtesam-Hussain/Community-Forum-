de03a249c4542b985b4cada41997f673ddcae37d
Ibtesam-Hussain <ibtesamh786@gmail.com>
2025-12-05 11:05:53 +0500

    testing cicd - updated Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,10 +8,10 @@ const Home = () => {
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
       <h1 className="text-4xl font-bold mb-6">Welcome to Community Q&A Forum</h1>
       <button
-        className="bg-blue-600 text-white px-6 py-3 rounded text-lg hover:bg-blue-700"
+        className="bg-green-600 text-white px-6 py-3 rounded text-lg hover:bg-blue-700"
         onClick={() => navigate("/auth")}
       >
-        Get Started
+        Get Started with us 
       </button>
     </div>
   );
